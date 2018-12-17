d05a274a50fb1c5287976ef9edad5c313f7c0ff1
bingo <bingo@192.168.25.4>
2018-12-17 21:38:37 +0900

    빌드에러 조치

--- src/main/java/com/chan/controller/BoardController.java
+++ src/main/java/com/chan/controller/BoardController.java
@@ -1,5 +1,7 @@
 package com.chan.controller;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,7 +28,8 @@ public class BoardController {
 	public void listAllGET(Model model) throws Exception{
 		
 		logger.info("listPageGET......Show Board's List");
-		
+		List<BoardVO> rtb = service.listPage();
+		System.out.println(rtb);
 		model.addAttribute("list", service.listPage());
 	}
 	
