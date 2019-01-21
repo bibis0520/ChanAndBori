c1f161263455d109f5b53b4c63d66cff8eb3f483
bibis0520 <kimchan0765@gmail.com>
2019-01-21 21:45:43 +0900

    1901212101 updated

--- src/test/java/com/chan/chanandbori/OracleConnectionTest.java
+++ src/test/java/com/chan/chanandbori/OracleConnectionTest.java
@@ -11,18 +11,17 @@ public class OracleConnectionTest {
 	private static final String URL = "jdbc:oracle:thin:@35.200.69.237:1521:XE";
 	private static final String USER = "CHAN";
 	private static final String PASSWORD = "1234";
-
 	@Test
 	public void testMySQLConnection() throws Exception {
-		
-		Class.forName(DRIVER); //Returns the Class object associated with the class or interface with the given string name. 
-		
+
+		Class.forName(DRIVER); //Returns the Class object associated with the class or interface with the given string name.
+
 		try ( Connection con = DriverManager.getConnection(URL, USER, PASSWORD) ){
-			
+
 			System.out.println(con);	//com.mysql.jdbc.JDBC4Connection@63e31ee
 
 		} catch ( Exception e ) {
-			
+
 			e.printStackTrace();
 		}
 
