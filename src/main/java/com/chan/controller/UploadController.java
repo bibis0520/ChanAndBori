e976dc291aefd94496c7ee8b384bbdcc370bde8f
bibis0520 <kimchan0765@gmail.com>
2019-02-04 21:34:16 +0900

    190204, 21:02 updated

--- src/main/java/com/chan/controller/UploadController.java
+++ src/main/java/com/chan/controller/UploadController.java
@@ -1,21 +1,28 @@
 package com.chan.controller;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.UUID;
 
 import javax.annotation.Resource;
 
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.chan.util.MediaUtils;
 import com.chan.util.UploadFileUtils;
 
 @Controller
@@ -25,6 +32,67 @@ public class UploadController {
 	private String uploadPath;
 
 	private static final Logger logger = LoggerFactory.getLogger(UploadController.class);
+
+	@ResponseBody
+	@RequestMapping("/displayFile")
+	public ResponseEntity<byte[]> displayFile(String fileName) throws Exception {
+
+		InputStream in = null;
+		ResponseEntity<byte[]> entity = null;
+
+		logger.info("FILE NAME : " + fileName);
+
+		try {
+			String formatName = fileName.substring(fileName.lastIndexOf(".")+1);
+
+			MediaType mType = MediaUtils.getMediaType(formatName);
+
+			HttpHeaders headers = new HttpHeaders();
+
+			in = new FileInputStream(uploadPath + fileName);
+
+			if ( mType != null ) {
+				headers.setContentType(mType);
+			} else {
+				fileName = fileName.substring(fileName.indexOf("_")+1);
+				headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+				headers.add("Content-Disposition", "attachment; filename=\""+ new String(fileName.getBytes("UTF-8"),"ISO-8859-1")+"\"");
+			}
+
+			entity = new ResponseEntity<byte[]>(IOUtils.toByteArray(in), headers, HttpStatus.CREATED);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			entity = new ResponseEntity<byte[]>(HttpStatus.BAD_REQUEST);
+		} finally {
+			in.close();
+		}
+
+		return entity;
+
+	}
+
+	@ResponseBody
+	@RequestMapping( value = "/deleteFile", method = RequestMethod.POST )
+	public ResponseEntity<String> deleteFile(String fileName) {
+
+		logger.info("DELETE FILE : " + fileName);
+
+		String formatName = fileName.substring(fileName.lastIndexOf(".")+1);
+
+		MediaType mType = MediaUtils.getMediaType(formatName);
+
+		if ( mType != null ) {
+			String front = fileName.substring(0, 12);
+			String end = fileName.substring(14);
+
+			new File(uploadPath + (front + end).replace('/', File.separatorChar)).delete();
+		}
+
+		new File(uploadPath + fileName.replace('/', File.separatorChar)).delete();
+
+		return new ResponseEntity<String>("deleted", HttpStatus.OK);
+	}
 
 // Ajax를 이용한 파일 업로드 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	@RequestMapping( value = "/uploadAjax", method = RequestMethod.GET )
@@ -43,8 +111,7 @@ public class UploadController {
 		logger.info("size : " + file.getSize());
 		logger.info("contentType : " + file.getContentType());
 																//HttpStatus.CREATED는 원하는 리소스가 정상적으로 생성되었다는 상태코드(HttpStatus.OK를 사용해도 무방)
-		return new ResponseEntity<>( UploadFileUtils.uploadFile(uploadPath, file.getOriginalFilename(), file.getBytes())
-								     , HttpStatus.CREATED );
+		return new ResponseEntity<>( UploadFileUtils.uploadFile(uploadPath, file.getOriginalFilename(), file.getBytes()) , HttpStatus.CREATED );
 	}
 
 // <iframe>을 이용한 파일 업로드 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
--- src/main/webapp/WEB-INF/views/uploadAjax.jsp
+++ src/main/webapp/WEB-INF/views/uploadAjax.jsp
@@ -27,6 +27,40 @@ small{
 </div>
 
 <script>
+	/**
+ 	* 정규표현식을 이용하여 파일의 확장자가 존재하는지를 검사함(i의 의미는 대, 소문자를 구분하지 않는다는 뜻)
+ 	*/
+	function checkImageType(fileName){
+		var pattern = /jpg$|gif$|png$|jpeg$/i;
+		return fileName.match(pattern);
+	};
+
+	/**
+	* jsp에서 출력되는 이름을 정해주는 함수
+	*/
+	function getOriginalName(fileName){
+		if ( checkImageType(fileName) ) {
+			return;
+		}
+
+		var idx = fileName.indexOf("_") + 1;
+		return fileName.substr(idx);
+	};
+
+	function getImageLink(fileName){
+		if ( !checkImageType(fileName) ) {
+			return;
+		}
+
+		var front = fileName.substr(0, 12);
+		var end = fileName.substr(14);
+
+		console.log("fileName : " + fileName);
+		console.log("front : " + front);
+		console.log("end : " + end);
+		return front + end;
+	}
+
 	$(".fileDrop").on("dragenter dragover", function(event){
 		event.preventDefault();
 	});
@@ -52,15 +86,49 @@ small{
 			contentType: false,
 			type: "POST",
 			success: function(data){
-				alert(data);
+
+				var str = "";
+
+				console.log(data);
+				console.log(checkImageType(data));
+
+				if ( checkImageType(data) ) {
+					str = "<div>"
+						+ "<a href='displayFile?fileName=" + getImageLink(data) + "'>"
+						+ "<img src='displayFile?fileName=" + data + "'/>"
+						+ "</a><small data-src=" + data + ">X</small></div>";
+				} else {
+					str = "<div><a href='displayFile?fileName=" + data + "'>"
+						+ getOriginalName(data)
+						+ "</a><small data-src=" + data + ">X</small></div>";
+				}
+
+				$(".uploadedList").append(str);
 			}
 		});
-
 		/*
 			processData : 데이터를 일반적인 query String으로 변환할 것인지를 결정, 기본값은 true로, "application/x-www-form-urlencoded" 타입으로 전송한다.
 						  다른 형식으로 데이터를 보내기 위해서 자동 변환하고 싶지 않은 경우는 false를 지정한다.
 			contentType : 기본값은 "application/x-www-form-urlencoded". 파일의 경우 multipart/form-data 방식으로 전송하기 위해서 false로 지정한다.
 		*/
+	});
+
+	$(".uploadedList").on("click", "small", function(event){
+
+		var that = $(this);
+
+		$.ajax({
+			url : "deleteFile",
+			type : "POST",
+			data : {fileName:$(this).attr("data-src")},
+			dataType : "text",
+			success : function(result){
+				if(result == 'deleted'){
+					that.parent("div").remove();
+				}
+			}
+		});
+
 	});
 </script>
 
